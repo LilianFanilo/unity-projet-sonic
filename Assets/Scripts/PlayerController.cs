7aa08893dce61f61cbcfbda41de5e7c899b23812
Lilian <lilianpeuron@gmail.com>
2024-01-29 08:21:52 +0100

    Add Long and Short jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     private const float GRAVITY_VALUE = -19.81f;
 
     [SerializeField] private float m_Speed = 5.0f;
-    [SerializeField] private float m_JumpHeight = 2.5f;
+    [SerializeField] private float m_JumpHeight = 5f;
+    [SerializeField] private float m_GravityMultiplier = 2f;
     [SerializeField] private float m_TurnSmoothTime = 0.1f;
 
     private CharacterController m_Character;
     private float m_JumpVelocity;
     private float m_TurnSmoothVelocity;
+    private float m_Gravity;
 
     private Vector2 m_MoveVector;
 
@@ -69,18 +71,28 @@ public class PlayerController : MonoBehaviour
 
     public void Jump()
     {
+        m_Gravity = GRAVITY_VALUE * m_GravityMultiplier;
+
+
+        //if (!m_Character.isGrounded && m_JumpVelocity > 0 && Input.GetButton("Jump") == true)
+        //{
+        //    Debug.Log("long jump");
+        //    m_JumpHeight = 5f;
+        //}
+
+        //if (!m_Character.isGrounded && m_JumpVelocity > 0 && Input.GetButton("Jump") == false)
+        //{
+            //Debug.Log("short jump");
+            //m_JumpHeight /= 2;
+
+        //} 
 
         if (!m_Character.isGrounded)
         {
             return;
         }
 
-        m_JumpVelocity += Mathf.Sqrt(m_JumpHeight * -GRAVITY_VALUE);
-
-        //if (Input.GetButton("Space"))
-        //{
-            Debug.Log("test");
-        //}
+        m_JumpVelocity += Mathf.Sqrt(m_JumpHeight * -3 * m_Gravity);
 
         m_Character.Move(Vector3.up * m_JumpHeight * Time.deltaTime);
 
@@ -89,6 +101,8 @@ public class PlayerController : MonoBehaviour
     private void ApplyGravity()
     {
         //TODO: Increment velocity with gravity only when character is grounded
+        m_Gravity = GRAVITY_VALUE * m_GravityMultiplier;
+
 
         if (m_Character.isGrounded && m_JumpVelocity < 0)
         {
@@ -96,7 +110,13 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        m_JumpVelocity += GRAVITY_VALUE * Time.deltaTime ;
+        if (!m_Character.isGrounded && m_JumpVelocity > 0 && Input.GetButton("Jump") == false)
+        {
+            //Debug.Log("short jump");
+            m_Gravity *= 2;
+        }
+
+        m_JumpVelocity += m_Gravity * Time.deltaTime ;
 
         m_Character.Move(Vector3.up * m_JumpVelocity * Time.deltaTime);
     }
