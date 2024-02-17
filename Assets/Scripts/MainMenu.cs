086fd15f7d811b1cf7977404a21427059aa4af8c
Lilian <lilianpeuron@gmail.com>
2024-02-17 09:57:52 +0100

    Add MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenuCanvas ;
+
+    void Awake()
+    {
+        if (_mainMenuCanvas == null)
+        {
+            return;
+        }
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            _mainMenuCanvas.SetActive(false);
+            return;
+        }
+        Time.timeScale = 0f;    
+    }
+
+    public void playButton()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void pauseButton()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void resumeButton() 
+    { 
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
