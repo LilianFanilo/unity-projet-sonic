862de78a7abd56c8dddfc520c6fd5acae91fd56a
Lilian <lilianpeuron@gmail.com>
2024-03-22 18:38:39 +0100

    Add DeathSound but not working

--- Assets/Scripts/GameRespawn.cs
+++ Assets/Scripts/GameRespawn.cs
@@ -6,11 +6,13 @@ using UnityEngine.PlayerLoop;
 public class GameRespawn : MonoBehaviour
 {
     public float threshold;
+    [SerializeField] private AudioClip DeathSoundClip;
 
     void FixedUpdate()
     {
         if (transform.position.y < threshold)
         {
+            AudioManager.instance.PlayClip(DeathSoundClip);
             transform.position = new Vector3(-0.01f, 10.22f, 0.05f);
         }
     }
